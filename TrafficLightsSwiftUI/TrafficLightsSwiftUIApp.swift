a1ece1f4207c776229fd76760e3703e93b1debd4
OlDiKi <delorian101allgood@gmail.com>
2022-02-16 09:41:39 +0300

    Initial Commit

---
+++ TrafficLightsSwiftUI/TrafficLightsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsSwiftUIApp.swift
+//  TrafficLightsSwiftUI
+//
+//  Created by Дмитрий Олейнер on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
